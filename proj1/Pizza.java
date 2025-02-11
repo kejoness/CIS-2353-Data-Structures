2fec89ed10bc1731186a3d259a54464f5448e59b
kejoness <kjones9843@gmail.com>
2025-02-11 08:01:41 -0500

    add vars and consts

--- proj1/Pizza.java
+++ proj1/Pizza.java
@@ -1,3 +1,27 @@
-public class Pizza {
+import java.util.ArrayList;
 
+public class Pizza {
+    enum CrustType {
+        Plain,
+        Butter,
+        Garlic,
+        GarlicButter,
+        Cheese
+    }
+
+    ArrayList<String> toppings = new ArrayList<String>();
+
+    enum SizeType {
+        Small,
+        Medium,
+        Large,
+        XLarge,
+        XXLarge,
+        Party
+    }
+
+    public Pizza() {
+        CrustType crust = CrustType.Plain;
+        SizeType size = SizeType.Small;
+    }
 }
