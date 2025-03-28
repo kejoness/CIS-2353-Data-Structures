066aea6e52a784b2255adec46c33b8c89942d494
kejoness <kjones9843@gmail.com>
2025-03-28 11:38:10 -0400

    create stack methods and custom exception class

---
+++ proj3/ChangeStack.java
@@ -0,0 +1,44 @@
+
+// Project 3
+// CIS 2353
+// Winter 2025
+// Prof. John P. Baugh
+
+public class ChangeStack {
+    private Node topNode;
+    private int numElements;
+
+    public ChangeStack() {
+        topNode = null;
+        numElements = 0;
+    }
+
+    // check if top of linked stack is empty
+    public boolean isEmpty() {
+        return topNode == null;
+    }
+
+    // add new node to top of linked stack
+    public void push(int item) {
+        topNode = new Node(item, topNode);
+    }
+
+    // look at top node of linked stack
+    public int peekTop() {
+        if(isEmpty()) {
+            throw new StackEmptyException("The stack is empty.");
+        } else {
+            return topNode.getData();
+        }
+    }
+
+    // remove the top node of linked stack
+    public int pop() {
+        if(isEmpty()) {
+            throw new StackEmptyException("The stack is empty.");
+        } else {
+            topNode = topNode.getNextNode();
+            return topNode.getData();
+        }
+    }
+}
---
+++ proj3/Node.java
@@ -0,0 +1,31 @@
+// Kayla Jones
+// Project 3
+// CIS 2353
+// Winter 2025
+// Prof. John P. Baugh
+
+public class Node {
+    private int data;
+    private Node nextNode;
+
+    public Node(int data, Node nextNode) {
+        this.data = data;
+        this.nextNode = nextNode;
+    }
+
+    public int getData() {
+        return data;
+    }
+
+    public void setData(int data) {
+        this.data = data;
+    }
+
+    public Node getNextNode() {
+        return nextNode;
+    }
+
+    public void setNextNode(Node nextNode) {
+        this.nextNode = nextNode;
+    }
+}
